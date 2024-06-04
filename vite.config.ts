87d5c658b73a4e36fc4ab57ae65c861caa606aad
Ubuntu <ubuntu@ip-172-31-6-221.ap-southeast-2.compute.internal>
2024-06-04 11:27:06 +0000

    update docker file and vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,8 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,  // Allows Vite to listen on all network interfaces
+    port: 8080   // Ensure this matches the port exposed in your Dockerfile
+  }
 });
